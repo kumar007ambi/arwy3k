9f87e5656638b3a366471971d33b183dc32e6d2f
kumar007ambi <nirajkewat011@gmail.com>
2023-11-24 19:25:39 +0530

    d

--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,40 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-name-editor',
   templateUrl: './name-editor.component.html',
   styleUrls: ['./name-editor.component.css']
 })
-export class NameEditorComponent implements OnInit {
-  userForm: FormGroup | any;
-  ngOnInit() {
-    this.userForm = new FormGroup({
-      firstName: new FormControl('', [Validators.required, Validators.minLength(15)]),
-      valNumber: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-      email: new FormControl('', [Validators.required, Validators.email]),
-    })
-  }
-
-  get firstName() {
-    return this.userForm.get('firstName');
-  }
-
-  get valNumber() {
-    return this.userForm.get('valNumber')
-  }
-  get email() {
-    return this.userForm.get('email')
-  }
-
+export class NameEditorComponent {
+  form = new FormGroup({
+    name : new FormControl(''),
+    val : new FormControl(''),
+    email : new FormControl(''),
+  })
+ 
+  
 
   // updateName() {
   //   this.name.setValue('Nancy');
   //   this.email.setValue('test@gamil.com'),
   // }
-  onSubmit() {
-    console.log(this.userForm.value)
-  }
+  // onSubmit() {
+  //   console.log()
+  // }
 }
 
 
